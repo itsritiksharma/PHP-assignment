a2297b9bc140e1d05cf157182b78cbd0860d3f2f
itsritiksharma <sritik216@gmail.com>
2021-03-18 11:12:11 +0530

    TRN-18:Added uploadErr variable to display errors and corrected the code flow

--- TRN-18/upload.php
+++ TRN-18/upload.php
@@ -1,3 +1,65 @@
+<?php
+
+//form script
+
+ //variables for first name and last name
+$firstNameErr = $lastNameErr =$fullNameErr = $uploadErr = "";
+$first_name = $last_name =$full_name = "";
+
+if ($_SERVER['REQUEST_METHOD']=="POST"){
+    //check if the input field is filled or not
+  if (empty($_POST["firstname"])) 
+    {
+      $firstNameErr = "first name is required";
+    }
+  elseif(preg_match('/^[^ ].* .*[^ ]$/',$first_name))
+    {
+      $firstNameErr = "First Name not valid!";
+    }
+  else 
+    {
+      $first_name = test_input($_POST["firstname"]);
+      // check if name only contains letters and whitespace
+      if (!preg_match("/^[a-zA-Z]*$/",$first_name)) 
+      {
+        $firstNameErr = "Only letters allowed";
+      }
+    }
+
+  if (empty($_POST["lastname"])) 
+    {
+      $lastNameErr = "last name is required";
+    } 
+  elseif(preg_match('/^[^ ].* .*[^ ]$/',$last_name))
+    {
+      $lastNameErr = "Last name not valid!";
+    }
+  else 
+  {
+    $last_name = test_input($_POST["lastname"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z]*$/",$last_name)) 
+    {
+      $lastNameErr = "Only letters allowed";
+    }
+    else
+    {
+        $full_name=$first_name." ".$last_name;
+
+    }
+  }
+
+}
+
+//function for test input
+
+function test_input($data){
+  $data=trim($data);
+  return $data;
+}
+
+?>
+
 
 <?php
 //file upload script
@@ -37,124 +99,24 @@ if (isset($_POST['submit'])) {
 				//destionation of file to be upladed at
 				$fileDest = '/var/www/PHP-assignment/TRN-18/uploads/'.$fileNameNew;
 				move_uploaded_file($fileTmpName, $fileDest);//moves the file to the desired location
-				echo "HOLA! your file is uploaded";
 
 				$full_name=$first_name." ".$last_name;
-
-				echo "<br>";
-
-				echo "Hello ".$full_name."!";
-
-				echo "<br>";
 								
 				
 				//header("Location: index.php?uploadsuccess");
 
 			}else{
-				echo "Your file is too big";
+				$uploadErr = "Your file is too big";
 			}
 		}else{
-			echo "There's an error uploading your file";
+			$uploadErr = "There's an error uploading your file";
 		}
 
 	}else{
-		echo "You cannot upload files of this type";
+		$uploadErr = "You cannot upload files of this type";
 	}
 
 }
 
 ?>
 
-
-<?php
-
-//form script
-
- //variables for first name and last name
-$nameErr=$full_name=$first_name=$last_name="";
-$firstNameErr = $lastNameErr =$fullNameErr = $uploadErr = "";
-
-
-if ($_SERVER['REQUEST_METHOD']=="POST"){
-    //check if the input field is filled or not
-  if (empty($_POST["firstname"])) 
-  {
-    $firstNameErr = "first name is required";
-  }
-  elseif(preg_match('/^[^ ].* .*[^ ]$/',$first_name))
-  {
-    echo "First Name not valid!";
-  }
-  else 
-  {
-    $first_name = test_input($_POST["firstname"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z]*$/",$first_name)) 
-    {
-      $firstNameErr = "Only letters allowed";
-    }
-  }
-
-  if (empty($_POST["lastname"])) 
-  {
-    $lastNameErr = "last name is required";
-  } 
-  elseif(preg_match('/^[^ ].* .*[^ ]$/',$last_name))
-  {
-    echo "Last name not valid!";
-  }
-  else 
-  {
-    $last_name = test_input($_POST["lastname"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z]*$/",$last_name)) 
-    {
-      $lastNameErr = "Only letters allowed";
-    }
-    else
-    {
-        $full_name=$first_name." ".$last_name;
-
-    }
-  }
-
-}
-
-//function for test input
-
-function test_input($data){
-  $data=trim($data);
-  return $data;
-}
-
-?>
-
-
-<?php
-
-$full_name=$first_name." ".$last_name;
-
-echo "<br>";
-
-echo "Hello ".$full_name."!";
-
-echo "<br>";
-?>
-
-
-
-<?php 
-
-//image display script
-
-$filename = "uploads/".$fileNameNew; 
-
-echo "<img src='".$filename."'>"
-
-?> 
-
-<?php
-echo "<br>";
-echo "Full Name: ".$full_name;
-echo "<br>";
-?>
