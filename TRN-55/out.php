e0d2b13783fbcf0f78819cac83cc73f695e0a7be
itsritiksharma <sritik216@gmail.com>
2021-03-23 21:29:22 +0530

    TRN-55:Corrected file format.

--- TRN-55/out.php
+++ TRN-55/out.php
@@ -2,16 +2,12 @@
 	if (isset($_POST['submit']))
 		{
 			$accessKey='2838541c68587385fa05e5ec91828728';
-
 			$email_address='support@apilayer.com';
-
 			$ch = curl_init('http://apilayer.net/api/check?access_key='.$accessKey.'&email='.$email.'');
-
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 			// Store the data:
 			$json = curl_exec($ch);
-
 			curl_close($ch);
 
 			// Decode JSON response:
@@ -22,45 +18,37 @@
 				{
 					echo "Email id is: ".$email;
 				}
-
 			else
 				{
 					$emailErr="Email not valid";
 				}
-			$validationResult['smtp_check'];
-			$validationResult['score'];
-
 			if (!($first_name&&$last_name&&$email))
 				{
-				  echo "Please fill all the details";
+				 	echo "Please fill all the details";
 				}
-
 			else
 				{
 
 				    echo "<br><br>";
 				    echo "Hello ".$full_name."!";
 				    echo "<br><br>";
- 
  				    echo "Phone number is: ".$phone;
-
 				    echo "<br><br>";
 				    $result=$_POST['marks'];
 				    foreach(explode("\n", $result) as $x)
 					    {
-					      $y[]=explode('|',$x);
+					    	$y[]=explode('|',$x);
 					    }
 
 				    echo "<table border=1; cellspacing=0>";
 					    for($z=0;$z<count($y);$z++)
 						    {
-						      echo "<tr>";
-						      echo "<td>".$y[$z][0]."</td>";
-						      echo "<td>".$y[$z][1]."</td";
-						      echo "</tr>";
+						    	echo "<tr>";
+						    	echo "<td>".$y[$z][0]."</td>";
+						    	echo "<td>".$y[$z][1]."</td";
+						    	echo "</tr>";
 						    }
 				    echo "</table>";
-
 				    $file=$_FILES['file'];
 				    $fileName=$_FILES['file']['name'];
 				    $fileSize=$_FILES['file']['size'];
@@ -75,72 +63,69 @@
 					//files to allow
 				    $allowed=array('jpg','jpeg','png');
 				    $fileNameNew = '';
+
 					//check the allowed extensions actually exists in the fileExt
 				    if (in_array($fileActualExt,$allowed))
-				    {
-						//check for an error
-					    if($fileError===0)
-						    {
-								//check file size
-						        if ($fileSize < 500000)
-							        {
-										//give file a unique name with the same extension
-								        $fileNameNew = uniqid('',true).".".$fileActualExt;
+					    {
+							//check for an error
+						    if($fileError===0)
+							    {
+									//check file size
+							        if ($fileSize < 500000)
+								        {
+											//give file a unique name with the same extension
+									        $fileNameNew = uniqid('',true).".".$fileActualExt;
 
-										//destionation of file to be upladed at
-								        $fileDest = '/var/www/PHP-assignment/TRN-55/uploads/'.$fileNameNew;
-										move_uploaded_file($fileTmpName, $fileDest);//moves the file to the desired location
-																				}
-				                        else
-					                        {
-					                          $uploadErr = "Your file is too big";
-					                        }
-							        }
-				                else
-				                    {
-				                       $uploadErr = "There's an error uploading your file";
-				                    }
-		                    }
-	                    else
-		                    {
-		                      $uploadErr = "You cannot upload files of this type";
-		                    }
-	                }
+											//destionation of file to be upladed at
+									        $fileDest = '/var/www/PHP-assignment/TRN-55/uploads/'.$fileNameNew;
+											move_uploaded_file($fileTmpName, $fileDest);//moves the file to the desired location
+										}
+			                        else
+				                        {
+				                        	$uploadErr = "Your file is too big";
+				                        }
+								}
+			                else
+			                    {
+			                    	$uploadErr = "There's an error uploading your file";
+			                    }
+			            }
+	                else
+	                    {
+	                      $uploadErr = "You cannot upload files of this type";
+	                    }
+	        	}
+         	$message = "<html>
+             				<body>
+             					<h2>Full name: ".$full_name."</h2>
+             					<h2>Email: ".$email."</h2>
+             					<h2>Table</h2><br>
+									<table border='1'>
+									<thead>
+									<tr>
+									<th>Subject Name</th>
+									<th> Marks</th>
+									</tr>
+									</thead>
+									<tbody>";
+									
+									for($i=0;$i<count($y);$i++)
+										{
+										  $message .="
+										  <tr>
+										  <td>" .$y[$i][0]. "</td>
+										  <td>" .$y[$i][1]. "</td>
+										  </tr>";
+										}
+									$message .="
+									</tbody>
+									</table>	
+								<img src ='/var/www/PHP-assignment/TRN-55/uploads/".$fileNameNew."' style='width:200px;height:200px'>
+							</body>
+					  	</html>";
 
-                 	$message = "<html>
-	                 				<body>
-	                 					<h2>Full name: ".$full_name."</h2>
-	                 					<h2>Email: ".$email."</h2>
-	                 					<h2>Table</h2><br>
-											<table border='1'>
-											<thead>
-											<tr>
-											<th>Subject Name</th>
-											<th> Marks</th>
-											</tr>
-											</thead>
-											<tbody>";
-											
-											for($i=0;$i<count($y);$i++)
-												{
-												  $message .="
-												  <tr>
-												  <td>" .$y[$i][0]. "</td>
-												  <td>" .$y[$i][1]. "</td>
-												  </tr>";
-												}
-											$message .="
-											</tbody>
-											</table>
-	          								
-										<img src ='/var/www/PHP-assignment/TRN-55/uploads/".$fileNameNew."' style='width:200px;height:200px'>
-									</body>
-							  	</html>";
-
-                 	$file2=fopen("details.doc","w+") or die("unabel to open file");
-                 	fwrite($file2, $message);				                
-                 	fclose($file2);
-
-                 	// echo '<a href="details.doc" download>Download details</a>';
+         	$file2=fopen("details.doc","w+") or die("unabel to open file");
+         	fwrite($file2, $message);				                
+         	fclose($file2);
 		}
 ?>
