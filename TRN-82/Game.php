8715a89bd3130354302dd90fcc595bf1c1f06fb1
itsritiksharma <sritik216@gmail.com>
2021-03-31 18:26:42 +0530

    TRN-82:Used constructor function to initialise variables and use them to access data and changed flag variable with meaningful variable.

--- TRN-82/Game.php
+++ TRN-82/Game.php
@@ -1,37 +1,44 @@
 <?php
   namespace RPSGame;
   class Game{
+    public $user_input;
+    public $game_options;
+    public $win_situations;
+    public $random_option;
+    public $computer;
+    function __construct(){
+      $this->user_input = strtolower($_POST['user_input']);
+      $this->game_options = ['rock', 'paper', 'scissors'];
+      $this->win_situations = ["rock"=>"scissors", "scissors"=>"paper", "paper"=>"rock"];
+      $this->random_option = array_rand($this->game_options);
+      $this->computer = $this->game_options[$this->random_option];
+    }
     function RPS(){
       if (isset($_POST['submit'])){
-        $user_input = strtolower($_POST['user_input']);
-        $game_options = ['rock', 'paper', 'scissors'];
-        $win_situations = ["rock"=>"scissors", "scissors"=>"paper", "paper"=>"rock"];
-        $random_option = array_rand($game_options);
-        $computer = $game_options[$random_option];
         if(empty($_POST['user_input'])){
           echo "Please input rock, paper or scissors";
         }
         else{
-          echo "your input ".$user_input;
+          echo "your move ".$this->user_input;
           echo '<br><br>';
-          echo "comp input ".$computer;
+          echo "comp move ".$this->computer;
           echo '<br><br>';
-          if($user_input===$computer){
+          if($this->user_input===$this->computer){
             echo "It's a Tie! Go again!";
           }
           else{
-            $flag=0;
-            foreach($win_situations as $key=>$value){
-              if ($user_input===$key&&$computer===$value){
-                $flag=1;
+            $match_result=0;
+            foreach($this->win_situations as $key=>$value){
+              if ($this->user_input===$key&&$this->computer===$value){
+                $match_result=1;
                 break;
               }
               else{
-                $flag= 0;
+                $match_result= 0;
                 continue;
               }
             }
-            if($flag==0){
+            if($match_result==0){
               echo "Try again!";
             }
             else{
