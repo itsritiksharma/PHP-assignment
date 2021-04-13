47b99949c50f6cbdc0f34fd3859d10ba708073f8
itsritiksharma <sritik216@gmail.com>
2021-04-14 00:02:42 +0530

    TRN-177:Added ajax script to post and calculate result.

--- TRN-177/index.php
+++ TRN-177/index.php
@@ -3,48 +3,36 @@
 <head>
   <title>Calculator</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script type="text/javascript">
+    function calc(){
+      var xhttp = new XMLHttpRequest();
+      var no1 = document.getElementById("input1").value;
+      var no2 = document.getElementById("input2").value;
+      var operator = document.getElementById("operator").value;
+
+      xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+          document.getElementById("status").innerHTML = this.responseText;
+        }
+
+      xhttp.open("POST", "calc.php", true);
+      xhttp.setRequestHeader("Content-type","application/x-www-from-urlencoded");
+      xhttp.send("n1=" + no1 + "&n2=" + no2 + "&op=" + operator);
+    }
+  </script>
 </head>
 <body>
-  <form method='post'>
-    <input type='number' id='first' name='first' >
-    <input type='number' id='second' name='second'>
-    <input type='submit' id='add' name='add' value='Add'>
-    <input type='submit' id='subtract' name='subtract' value='Subtract'>
-    <input type='submit' id='multiply' name='multiply' value='Multiply'>
-    <input type='submit' id='divide' name='divide' value='Divide'>
-  </form>
-  <div id='displayresult'></div>
-  <script type="text/javascript">
-    $(document).ready(function () {
-      $('#add').on('click', function () {
-        var n1 = parseInt($('#first').val());
-        var n2 = parseInt($('#second').val());
-        var r = n1 + n2;
-        $('#displayresult').html("<h3>Sum= " + r + "</h3>");
-        return false;
-      });
-      $('#subtract').on('click', function () {
-        var n1 = parseInt($('#first').val());
-        var n2 = parseInt($('#second').val());
-        var r = n1 - n2;
-        $('#displayresult').html("<h3>Subtract= " + r + "</h3>");
-        return false;
-      });
-      $('#multiply').on('click', function () {
-        var n1 = parseInt($('#first').val());
-        var n2 = parseInt($('#second').val());
-        var r = n1 * n2;
-        $('#displayresult').html("<h3>Multiply= " + r + "</h3>");
-        return false;
-      });
-      $('#divide').on('click', function () {
-        var n1 = parseInt($('#first').val());
-        var n2 = parseInt($('#second').val());
-        var r = n1 / n2;
-        $('#displayresult').html("<h3>Division= " + r + "</h3>");
-        return false;
-      });
-    });
-  </script>
+  First Input:<input type="number" name="input1" id="input1"><br>
+  Second Input:<input type="number" name="input2" id="input2"><br>
+  Operator:<select id="operator">
+    <option>--Select An Operator--</option>
+    <option>+</option><br/>
+    <option>-</option>
+    <option>*</option>
+    <option>/</option>
+  </select><br><br>
+  <!-- <button type="button" onclick="window.calc(); return false;">Calculate</button> -->
+  <button type="button" onclick="calc()">Calculate</button>
+  <div id="status"></div>
 </body>
 </html>
