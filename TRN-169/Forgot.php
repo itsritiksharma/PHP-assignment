72cf2b0dd44e26b75d0bd92035560a308ec4a3a8
itsritiksharma <sritik216@gmail.com>
2021-04-13 11:16:30 +0530

    TRN-169:Added class and method to change the password of user.

---
+++ TRN-169/Forgot.php
@@ -0,0 +1,34 @@
+<?php
+
+  /**
+  *
+  * @file
+  *
+  * Change the password in the database.
+  *
+  */
+
+  namespace ForgotPassword;
+
+  class Forgot{
+    public $conn;
+    // Set connection variable
+    public function __construct($conn){
+      $this->conn = $conn;
+    }
+
+    /**
+    *
+    * Changes the password in the database.
+    *
+    * @param array of strings $data, string $newencpassword [$data contains data
+    * from form submission and $newencpassword contains the new encrypted password]
+    *
+    */
+    public function changePassword($data, $newencpassword){
+      // Query to update password column for the Users
+      $query = "UPDATE Users SET password = '" . $newencpassword . "' WHERE email = '" . $data['email'] . "';";
+      $user = $this->conn->query($query);
+      echo "Password updated Successfully!<br><br>";
+    }
+  }
