74bd02b68fa46a98b88abd3c181876d116a73800
itsritiksharma <sritik216@gmail.com>
2021-04-14 14:08:41 +0530

    TRN-177:Updated variable names.

--- TRN-177/calc.php
+++ TRN-177/calc.php
@@ -1,34 +1,44 @@
 <?php
- $i1 = $_REQUEST['n1'];
- $i2 = $_REQUEST['n2'];
- $op = $_REQUEST['op'];
- switch($op)
-  {
+
+  /**
+  *
+  * @file
+  *
+  * Request incoming numbers and operator and echo the desired output once match found.
+  *
+  */
+
+  // Request incoming parameters
+  $no1 = $_REQUEST['n1'];
+  $no2 = $_REQUEST['n2'];
+  $op = $_REQUEST['op'];
+  // Run switch case on operator to echo the output
+  switch($op){
     case "--Select An Operator--":
       $output = "Choose valid operator";
       echo $output;
       break;
-
-    case "+":
-      $output = $i1 + $i2;
+    // If user chooses Add then add the numbers and echo
+    case 'Add':
+      $output = $no1 + $no2;
       echo $output;
       break;
-
-    case "-":
-      $output = $i1 - $i2;
+    // If user chooses Subtract then echo the difference of numbers.
+    case 'Subtract':
+      $output = $no1 - $no2;
       echo $output;
       break;
-
-    case '*':
-      $output = $i1 * $i2;
+    // If user chooses Multiply then echo the product of the numbers.
+    case 'Multiply':
+      $output = $no1 * $no2;
       echo $output;
       break;
-
-    case '/':
-      $output = $i1 / $i2;
+    // If user chooses Divide then echo the division of the numbers.
+    case 'Divide':
+      $output = $no1 / $no2;
       echo $output;
       break;
-
+    // If none of these is true run default case
     default:
       echo $output = "Not Found...Please Enter only Integers or Decimals...";
   }
