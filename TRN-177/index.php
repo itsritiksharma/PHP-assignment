49b364758dac337bf63697ce5c088e59eaefea04
itsritiksharma <sritik216@gmail.com>
2021-04-14 14:16:49 +0530

    TRN-177:Added comments.

--- TRN-177/index.php
+++ TRN-177/index.php
@@ -1,12 +1,25 @@
 <?php
+
+  /**
+  *
+  * @file
+  *
+  * echo the operating system PHP is running on, PHP browser detection script. PHP script, to check whether
+  * the page is called from 'https' or 'http'.
+  *
+  */
+
+  // Echo Operating system details
   echo "<h2>Operating System</h2>";
   echo php_uname();
   echo '<br>';
+  // Echo Browser details
   echo "<h2>Browser</h2>";
   echo $_SERVER['HTTP_USER_AGENT'];
   $browser = get_browser(null, true);
   print_r($browser);
   echo '<br>';
+  // Check whether the page is called from 'https' or 'http'.
   echo "<h2>HTTP or HTTPS?</h2>";
   if (!empty($_SERVER['HTTPS'])){
     echo 'https is enabled';
