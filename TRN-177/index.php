680ec452331e18b7dad6a368f75d1bab6f40568f
itsritiksharma <sritik216@gmail.com>
2021-04-16 14:19:22 +0530

    TRN-177:Changed the UI of calculator.

--- TRN-177/index.php
+++ TRN-177/index.php
@@ -1,46 +1,85 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Calculator</title>
-  <!-- Source to Jquery script -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <!-- Script to send the input variable and operator to the calc.php file -->
-  <script type="text/javascript">
-    function calc(){
-      // Create new XMLHttpRequest object.
-      var xhttp = new XMLHttpRequest();
-      // Fetch the values in the selected Ids.
-      var no1 = document.getElementById("input1").value;
-      var no2 = document.getElementById("input2").value;
-      var operator = document.getElementById("operator").value;
-      // Whenever the state changes to the desired value run the function to print output.
-      xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-          document.getElementById("result").innerHTML = this.responseText;
-        }
-      }
-      // Make a post request to calc.php
-      xhttp.open("POST", "calc.php", true);
-      // Set the headers
-      xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-      // Send the data in headers.
-      xhttp.send("n1=" + no1 + "&n2=" + no2 + "&op=" + operator );
-    }
+  <title>Ajax Calculator</title>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <script>
+    $(document).ready(function(){
+      //general
+      $('input[type=button]').click(function(){
+        var num = $(this).val();
+        var old = $('#display').html();
+          //this will clear the screen
+          if( num === 'C' ){
+            $('#display').html('');
+            return;
+          }
+          if( num === '=' ){
+            $('#display').html(old);
+            return;
+          }
+          $.ajax({
+            url:'ajax.php',
+            type: "POST",
+            data:{'action':'operation','num':num,'old':old},
+            success: function(msg){
+              $('#display').html(msg);
+            }
+          }).error(function(){
+            $('#display').html('Oops! An error occured');}
+            );
+        });
+      //equal button click
+      $('#eql').click(function(){
+        var num = $('#display').html();
+        var old = $('#display').html();
+        $.ajax({
+          url:'ajax.php',
+          type: "POST",
+          data:{'action':'equal', 'num':num, 'old':old},
+          success: function(msg){
+            $('#display').html(msg);
+          }
+        }).error(function(){
+          $('#display').html('Oops! An error occured');}
+          );
+      });
+    });
   </script>
 </head>
 <body>
-  First Input:<input type="number" name="input1" id="input1"><br>
-  Second Input:<input type="number" name="input2" id="input2"><br>
-  Operator:<select id="operator">
-    <option>--Select An Operator--</option>
-    <option>Add</option>
-    <option>Subtract</option>
-    <option>Multiply</option>
-    <option>Divide</option>
-  </select><br><br>
-  <!-- On Clicking the button it calls the calc() function defined in script -->
-  <button type="button" onclick="calc()">Calculate</button><br><br>
-  <!-- Print the final result -->
-  Result: <span id="result"></span>
+  <table style="width:0;text-align:center;margin:0 auto"  class="table table-borderless">
+    <tr>
+      <td colspan="5"><textarea id="display" class="display"></textarea></td>
+    </tr>
+    <tr>
+      <td><input value="7" type="button" class="btn btn-light"></td>
+      <td><input value="8" type="button" class="btn btn-light"></td>
+      <td><input value="9" type="button" class="btn btn-light"></td>
+      <td><input id="div" value="/" type="button" class="but"></td>
+    </tr>
+    <tr>
+      <td><input value="4" type="button" class="btn btn-light"></td>
+      <td><input value="5" type="button" class="btn btn-light"></td>
+      <td><input value="6" type="button" class="btn btn-light"></td>
+      <td><input id="sub" value="-" type="button" class="but"></td>
+    </tr>
+    <tr>
+      <td><input value="1" type="button" class="btn btn-light"></td>
+      <td><input value="2" type="button" class="btn btn-light"></td>
+      <td><input value="3" type="button" class="btn btn-light"></td>
+      <td><input id="mul" value="*" type="button" class="but"></td>
+    </tr>
+    <tr>
+      <td><input value="0" type="button" class="btn btn-light"></td>
+      <td><input id="dot" value="." type="button" class="btn btn-light"></td>
+      <td><input id="plus" value="+" type="button" class="but"></td>
+    </tr>
+    <tr>
+      <td><input id="cls" value="C" type="button" class="btn btn-danger btn-lg"></td>
+      <td><input id="eql" value="=" type="button" class="btn btn-success btn-lg"></td>
+    </tr>
+  </table>
 </body>
 </html>
